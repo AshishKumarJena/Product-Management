382adf7d6da472e0ee5a262b636962265c13e09c
AshishKumarJena <ashishkujena2000@gmail.com>
2022-07-26 01:11:43 +0530

    Added Product Management Microservice

---
+++ src/main/java/com/cts/productmanagement/ProductManagementApplication.java
@@ -0,0 +1,28 @@
+package com.cts.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+			}
+		};
+	}
+
+}
---
+++ src/main/java/com/cts/productmanagement/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.cts.productmanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.productmanagement.feign.AuthClient;
+import com.cts.productmanagement.model.AppProduct;
+import com.cts.productmanagement.model.JwtResponse;
+import com.cts.productmanagement.service.ProductService;
+
+
+@RestController
+@CrossOrigin
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private AuthClient authClient;
+		
+	
+    @PostMapping("/product")
+	public AppProduct addProduct(@RequestBody AppProduct appProduct) {    	
+    	return productService.addProduct(appProduct);
+	}
+    
+    @PutMapping("/product/{id}")
+	public AppProduct updateProduct(@RequestBody AppProduct appProduct) {
+		return  productService.updateProduct(appProduct);
+	}
+  
+    @GetMapping("/product")
+	public List<AppProduct> getProducts(){
+		return productService.getProduct();		
+	}	
+    
+    @GetMapping("/product/{id}")
+	public AppProduct getProductById(@PathVariable("id") int id) {		
+		return productService.getProductById(id);
+	}
+    
+    @GetMapping("/awsProduct")
+	public String welcome() {
+    	return "Product Deployed to Cloud";
+	}
+    
+    @DeleteMapping("/product/{id}")
+	public void deleteProduct(@PathVariable int id) {
+		productService.deleteProduct(id);
+	}
+	
+	
+	
+}
